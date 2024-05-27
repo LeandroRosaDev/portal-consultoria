b1a423354cb63e9c63fa004e6b05ef3216f2d7e1
Icaro Pecinalli <icaropecinalli@hotmail.com>
2024-05-27 20:49:36 -0300

    feat: criação do editar cadastro

--- src/actions/user/user-put-action.ts
+++ src/actions/user/user-put-action.ts
@@ -1,28 +1,32 @@
-'user serve';
+'use server';
+
 import apiError from '@/functions/api-error';
 import { url } from '@/functions/url';
-import getToken from '../get-token-action';
+import { cookies } from 'next/headers';
 
 export async function editUserInfo(
   state: {},
   formData: FormData,
   passwordConfirmation: string,
 ) {
-  const username = (formData.get('username') as string) || null;
-  const newPassword = (formData.get('newPassword') as string) || null;
+  const token = cookies().get('token')?.value;
+  const username = formData.get('username') as string | null;
+  const newPassword = formData.get('newPassword') as string | null;
 
   try {
-    if (!username || !newPassword || !passwordConfirmation)
-      throw new Error('Preencha os dados.');
+    if (!username && !newPassword) throw new Error('Preencha os dados.');
+    if (!passwordConfirmation)
+      throw new Error('Confirmação de senha necessária.');
+    if (username === null) throw new Error('Nome de usuário não fornecido.');
 
     const confirmed = await confirmUserPassword(username, passwordConfirmation);
 
     if (!confirmed) throw new Error('Senha incorreta.');
 
-    const response = await fetch(url + '/sua-rota-de-edicao-de-usuario', {
+    const response = await fetch(url + '/wp-json/api/usuario', {
       method: 'PUT',
       headers: {
-        Authorization: 'Bearer ' + getToken,
+        Authorization: 'Bearer ' + token,
       },
       body: formData,
     });
@@ -41,26 +45,22 @@ async function confirmUserPassword(
   password: string,
 ): Promise<boolean> {
   try {
-    const response = await fetch(
-      url + '/wp-json/custom-auth/v1/confirm-password',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
+    const response = await fetch(url + '/wp-json/jwt-auth/v1/token/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
 
     if (!response.ok) throw new Error('Falha ao confirmar a senha.');
 
     const data = await response.json();
-    console.log(data);
 
-    return data.passwordConfirmed === true;
+    return data.token ? true : false;
   } catch (error) {
     console.error('Erro ao confirmar senha:', error);
     return false;
